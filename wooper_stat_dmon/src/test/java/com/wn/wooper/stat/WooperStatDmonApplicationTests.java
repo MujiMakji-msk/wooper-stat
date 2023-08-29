4e1f7a253cebfc362bc72e28b53c003f95f987cf
kimminsoo <kimminsoo@localhost>
2023-08-29 14:36:24 +0900

    우퍼 통계 패치 데몬 초기 등록

---
+++ wooper_stat_dmon/src/test/java/com/wn/wooper/stat/WooperStatDmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wn.wooper.stat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooperStatDmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
